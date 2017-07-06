ca26358702871aa87682f151be1358f8708db5f4
Joseph Botros <jrbotros@gmail.com>
2017-07-06 12:18:10 -0400

    fix(api): pass updated params to API methods

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 import request from 'superagent';
 
-export function load() {
+export function load(contextKey) {
   return request
     .get('https://your-api.here/get-annotation-mappings/')
+    .query({ contextKey })
     .then(response => response.body);
 }
 
-export function save(state) {
+export function save(updatedMappings, contextKey) {
   return request
     .post('https://your-api.here/save-annotation-mappings/')
-    .send(state)
+    .send({ updatedMappings, contextKey })
     .then(response => response.body);
 }
